44476e2270f8b0e89a8b0f7748bd9c673c6d5f2e
Dan <daniilk23567@gmail.com>
2024-09-25 08:40:14 +0300

    Создаем корзину

--- home/static/home/script.js
+++ home/static/home/script.js
@@ -1,14 +1,13 @@
 const saveToLocalStoreage = (key, data) => {
-    localStorage.setItem(key, data)
+    localStorage.setItem(key, JSON.stringify(data)) // сохрание в лк
 }
 
 const getFromStorage = (key, default_n) => {
-    return JSON.parse(localStorage.getItem(key)) || default_n
+    return JSON.parse(localStorage.getItem(key)) || default_n // условие
 }
 
 const onLoad = () => {
     const cart = getFromStorage('cart', [])
-    console.log(cart) // проверка возвращения массива
 }
 
 document.addEventListener(
